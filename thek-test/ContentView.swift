310c6f4a6709421a0d863952baed019129f9e81a
Hyunseo <rlagustj07@ewha.ac.kr>
2026-05-19 11:07:01 +0900

    묭의 첫번째 커밋

--- thek-test/ContentView.swift
+++ thek-test/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("묭이 수정한 텍스트")
         }
         .padding()
     }
