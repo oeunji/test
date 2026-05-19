bb944448306f574c028033db0e8b6b3700ec9aff
oeunji <oieunxe@gmail.com>
2026-05-19 10:49:15 +0900

    Initial Commit

---
+++ thek-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thek-test
+//
+//  Created by 이은지 on 5/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ thek-test/thek_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  thek_testApp.swift
+//  thek-test
+//
+//  Created by 이은지 on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct thek_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
